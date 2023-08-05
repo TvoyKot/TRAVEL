f55e853601fe38967db56e57a8ff19cf7d14d840
TvoyKot <a.lshe@yandex.by>
2023-08-05 11:19:28 +0300

    done

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,19 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<button type = "button" class = "article-slider__arrow article-slider__arrowleft"><img src="images/svg/arrow-slider-left.svg" alt="arrow left"></button>',
-        nextArrow: '<button type = "button" class = "article-slider__arrow article-slider__arrowright"><img src="images/svg/arrow-slider-right.svg" alt="arrow right"></button>'
+        nextArrow: '<button type = "button" class = "article-slider__arrow article-slider__arrowright"><img src="images/svg/arrow-slider-right.svg" alt="arrow right"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     })
 
     $('.menu-btn').on('click', function () {
-        $('.menu-btn').toggleClass('menu-btn--active')
+        $('.menu-phone,.menu-btn').toggleClass('active')
     })
 })
 
